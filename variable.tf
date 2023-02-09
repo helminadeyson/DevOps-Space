800b1c2613b0681649468481746cf85978c89303
joe <joehaddy07@gmail.com>
2023-02-09 13:12:05 -0500

    updated variable file

--- variable.tf
+++ variable.tf
@@ -11,3 +11,9 @@ variable "pub_subnet_1a_cidr" {
     description = "cidr for public subnet 1a"
     default = "10.0.0.0/24"
 }
+
+variable "pub_subnet_1b_cidr" {
+    type = string
+    description = "cidr for public subnet 1b"
+    default = "10.0.1.0/24"
+}

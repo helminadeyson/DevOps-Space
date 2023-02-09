228b6665634dffc943e494ce83477946913b8e81
helmina deyson <helminadeyson@gmail.com>
2023-02-09 17:56:59 +0000

    variable for public subnet 1a cidr

--- variable.tf
+++ variable.tf
@@ -5,3 +5,9 @@ variable "vpc_cidr" {
     description = "cidr for dev vpc"
     default = "10.0.0.0/16"
 }
+
+variable "pub_subnet_1a_cidr" {
+    type = string
+    description = "cidr for public subnet 1a"
+    default = "10.0.0.0/24"
+}

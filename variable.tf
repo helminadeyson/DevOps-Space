9ff6ce694fef329512b4289b3d268739289fd075
joe <joehaddy07@gmail.com>
2023-02-09 20:15:48 -0500

    private subnet 1a

--- variable.tf
+++ variable.tf
@@ -17,3 +17,9 @@ variable "pub_subnet_1b_cidr" {
     description = "cidr for public subnet 1b"
     default = "10.0.1.0/24"
 }
+
+variable "private_subnet_1a_cidr" {
+    type = string
+    description = "cidr for private subnet 1a"
+    default = "10.0.2.0/24"
+}
